91392b3353b94f233560aa288a98dfac2107271d
Vincent Jeltsch <jeltsch.vincent@gmail.com>
2018-07-05 22:34:31 +0200

    Adds signature validation middleware

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
+const crypto = require('crypto');
 const { exec } = require('child_process');
 
 app = express();
@@ -12,6 +13,21 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 
+//Middleware to verify that the signature is valid.
+app.use(function(req, res, next) {
+	//TODO: Add proper secret from an Environment Variable
+	hmac = crypto.createHmac('sha1', 'test-secret');
+	hmac.update(JSON.stringify(req.body));
+	calculatedSignature = 'sha1=' + hmac.digest('hex');
+
+	if (crypto.timingSafeEqual(req.headers['x-hub-signature'], calculatedSignature)) {
+		next();
+	} else {
+		console.log("Bad singature on this request: " + req.headers['x-hub-signature']);
+		res.status(403).send("Bad signature.");
+	}
+});
+
 app.get('/', function(req, res) {
 	res.send("The webhook is listening for changes on /payload.");
 });
@@ -22,15 +38,18 @@ app.get('/payload', function(req, res) {
 
 app.post('/payload', function(req, res) {
 	
+	//We only do something on pushes to master.
 	if (req.body.ref !== "refs/heads/master") {
 		return res.send("Push was not to master branch. Will be ignored.");
 	}
 
+	//If the pusher is known.
 	if (req.body.pusher && req.body.pusher.name && req.body.pusher.email) {
 		console.log(req.body.pusher.name + " (" + req.body.pusher.email + ") has pushed to master branch:");
 		if(req.body.commits.length > 0) {
 			console.log('"' + req.body.commits[0].message + '"');
 		}
+		//Fetch, Build and run the project
 		exec("sublime ~/Work/klecksx/capacitor-lock", function(error, stdout, stderr) {
 			if (error) {
 		    console.error(`exec error: ${error}`);
