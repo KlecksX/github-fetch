29b7ed77d19e376bce5edbcb7cd08db2130d0980
Vincent Jeltsch <jeltsch.vincent@gmail.com>
2018-07-07 20:56:37 +0200

    Adds automatic ngrok tunnel creation

--- app.js
+++ app.js
@@ -2,9 +2,13 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
 
 const crypto = require('crypto');
+const cheerio = require('cheerio');
 const { exec } = require('child_process');
+const execSync = require('child_process').execSync;
+const spawn = require('child_process').spawn;
 
 const PORT = process.env.PORT || 3000;
 
@@ -12,6 +16,21 @@ const GitHubFetcher = require('./GitHubFetcher');
 
 var app = express();
 var githubFetcher = new GitHubFetcher('https://github.com/KlecksX/github-fetch.git', 'test', 'test-secret');
+
+//Start ngrok and
+const ngrok = exec('ngrok http 3000', githubFetcher.execCallback);
+
+request('http://localhost:4040/status', function(error, response, body) {
+	if (error) {
+		console.log('Error occured fetching ngrok url.');
+
+		ngrok.kill();
+		console.log('Ngrok has been terminated, please start it manually.');
+	}
+	var ngrokUrl = body.match(/https:\/\/([a-zA-Z0-9]*).ngrok.io/)[0];
+	console.log('Paste this line as a github webhook for your repository:');
+	console.log(ngrokUrl + '/payload');
+});
 
 // configure the app to use bodyParser()
 app.use(bodyParser.urlencoded({
